60551f88c0f009b48b36012347f04b5cceb5c50f
Jesus Moreno Amor <jesus@morenoamor.com>
2014-06-26 10:00:53 +0200

    Cleanup

--- parsespool.py
+++ parsespool.py
@@ -1,20 +1,8 @@
 ################################################################################
 # -*- encoding: utf-8 -*-
 ################################################################################
-import os
 import re
-import sys
 from inflection import *
-
-################################################################################
-# Logging
-################################################################################
-import logging
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)-7s - %(message)s',
-    level=logging.INFO,
-    datefmt='%Y/%m/%d %H:%M:%S'
-)
 
 def generate_xml(lines):
     for line in lines:
@@ -54,23 +42,17 @@ def generate_xml(lines):
             else:
                 print column
 
-getter_method = """  public %s get%s()
-  {
-    return %s;
-  }
-
+getter_method = """  public {return_type} get{method_name}()
+  {{
+    return this.{attribute_name};
+  }}
 """
 
-setter_method = """  public void set%s(%s %s)
-  {
-    this.%s = %s;
-  }
-
+setter_method = """  public void set{method_name}({attribute_type} {attribute_name})
+  {{
+    this.{attribute_name} = {attribute_name};
+  }}
 """
-
-def convert(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 def generate_annotations(lines):
     missing = list()
@@ -98,7 +80,9 @@ def generate_annotations(lines):
                     fw.write('}' + '\n')
                     fw.close()
 
-                fw = open('/tmp/{table}.java'.format(table=camelize(underscore(C_TABLA))), 'w')
+                J_CLASE = camelize(underscore(C_TABLA))
+
+                fw = open('/tmp/{table}.java'.format(table=J_CLASE), 'w')
 
                 fw.write('package com.morenoamor.vodafone.ial;' + '\n')
                 fw.write('' + '\n')
@@ -110,80 +94,53 @@ def generate_annotations(lines):
                 fw.write('' + '\n')
                 fw.write('@Entity' + '\n')
                 fw.write('@Table(name="{table}")'.format(table=C_TABLA) + '\n')
-                fw.write('public class {table}'.format(table=camelize(underscore(C_TABLA))) + '\n')
+                fw.write('public class {table}'.format(table=J_CLASE) + '\n')
                 fw.write('{' + '\n')
 
             last_table = C_TABLA
 
+            J_ATRIBUTO = camelize(underscore(C_NOMBRE.replace('FX_', 'FECHA_')), False)
+            J_METODO = camelize(underscore(C_NOMBRE.replace('FX_', 'FECHA_')))
+            J_TIPO = None
+
             if C_TIPO == 'VARCHAR2' or C_TIPO == 'CHAR':
                 J_TIPO = "String"
-                J_ATRIBUTO = camelize(underscore(C_NOMBRE))
-                J_METODO = camelize(underscore(C_NOMBRE), False)
 
-                fw.write('  @Column(name="%s", length=%s, nullable=%s)' % (C_NOMBRE, C_SIZE, str(C_NULLABLE == 'Y').lower()) + '\n')
-                fw.write('  private %s %s;' % (J_TIPO, C_NOMBRE) + '\n')
-                methods.append(getter_method % (J_TIPO, J_METODO, J_ATRIBUTO))
-                methods.append(setter_method % (C_NOMBRE, J_TIPO, J_METODO, J_ATRIBUTO, J_ATRIBUTO))
             elif C_TIPO == 'NUMBER':
-                fw.write('  @Column(name="%s", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
-                fw.write('  private int %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('int', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'int', C_NOMBRE, C_NOMBRE, C_NOMBRE))
+                J_TIPO = "int"
+
             elif C_TIPO == 'LONG':
-                fw.write('  @Column(name="%s", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
-                fw.write('  private long %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('long', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'long', C_NOMBRE, C_NOMBRE, C_NOMBRE))
+                J_TIPO = "long"
+
             elif C_TIPO == 'DATE':
-                fw.write('  @Column(name="%s", columnDefinition="DATE", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
-                fw.write('  private Date %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('Date', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'Date', C_NOMBRE, C_NOMBRE, C_NOMBRE))
+                J_TIPO = "Date"
+
             elif C_TIPO == 'TIMESTAMP(':
-                fw.write('  @Column(name="%s", columnDefinition="DATETIME", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
-                fw.write('  private Date %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('Date', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'Date', C_NOMBRE, C_NOMBRE, C_NOMBRE))
+                J_TIPO = "Date"
+
             elif C_TIPO == 'CLOB':
-                fw.write('  @Column(name="%s", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
+                J_TIPO = "String"
                 fw.write('  @Lob(type = LobType.CLOB)' + '\n')
-                fw.write('  private String %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('String', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'String', C_NOMBRE, C_NOMBRE, C_NOMBRE))
+
             elif C_TIPO == 'BLOB':
-                fw.write('  @Column(name="%s", nullable=%s)' % (
-                    C_NOMBRE,
-                    str(C_NULLABLE == 'Y').lower(),
-                ) + '\n')
+                J_TIPO = "byte[]"
                 fw.write('  @Lob(type = LobType.BLOB)' + '\n')
-                fw.write('  private byte[] %s;' % (C_NOMBRE) + '\n')
-                methods.append(getter_method % ('byte[]', C_NOMBRE, C_NOMBRE))
-                methods.append(setter_method % (C_NOMBRE, 'byte[]', C_NOMBRE, C_NOMBRE, C_NOMBRE))
             else:
                 missing.append(column)
+                continue
+
+            fw.write('  @Column(name="%s", length=%s, nullable=%s)' % (C_NOMBRE, C_SIZE, str(C_NULLABLE == 'Y').lower()) + '\n')
+            fw.write('  private %s %s;' % (J_TIPO, J_ATRIBUTO) + '\n')
+            methods.append(getter_method.format(return_type=J_TIPO, method_name=J_METODO, attribute_name=J_ATRIBUTO))
+            methods.append(setter_method.format(method_name=J_METODO, attribute_type=J_TIPO, attribute_name=J_ATRIBUTO))
 
         fw.write('' + '\n')
 
     for c in missing:
         print c
 
-#with open("/cygdrive/c/cygwin/home/L0009813/GoogleDrive/12 IAL/Esquemas BBDD/Esquema_IAL") as f:
-with open("/cygdrive/c/cygwin/home/L0009813/GoogleDrive/12 IAL/Esquemas BBDD/Esquema_Comanche") as f:
+with open("/cygdrive/c/cygwin/home/L0009813/GoogleDrive/12 IAL/Esquemas BBDD/Esquema_IAL") as f:
+#with open("/cygdrive/c/cygwin/home/L0009813/GoogleDrive/12 IAL/Esquemas BBDD/Esquema_Comanche") as f:
 
     tablas = list()
 
